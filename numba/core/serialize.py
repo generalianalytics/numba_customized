d481dcaab348795b5457c046f4977ab46e9b2f91
generalianalytics <93379591+generalianalytics@users.noreply.github.com>
2021-11-02 19:25:57 +0100

    Update serialize.py

--- numba/core/serialize.py
+++ numba/core/serialize.py
@@ -154,13 +154,6 @@ def _no_pickle(obj):
 
 def disable_pickling(typ):
     """This is called on a type to disable pickling
-class SlowNumbaPickler(pickle._Pickler):
-    """Extends the pure-python Pickler to support the pickling need in Numba.
-    Adds pickling for closure functions, modules.
-    Adds customized pickling for _CustomPickled to avoid invoking a new
-    Pickler instance.
-    Note: this is used on Python < 3.8 unless `pickle5` is installed.
-    Note: This is good for debugging because the C-pickler hides the traceback
     """
     NumbaPickler.disabled_types.add(typ)
     # The following is needed for Py3.7
@@ -212,76 +205,15 @@ class ReduceMixin(abc.ABC):
     def __reduce__(self):
         return custom_reduce(self._reduce_class(), self._reduce_states())
 
+
 class PickleCallableByPath:
     """Wrap a callable object to be pickled by path to workaround limitation
     in pickling due to non-pickleable objects in function non-locals.
-
     Note:
     - Do not use this as a decorator.
     - Wrapped object must be a global that exist in its parent module and it
       can be imported by `from the_module import the_object`.
-# ----------------------------------------------------------------------------
-# The following code is adapted from cloudpickle as of
-# https://github.com/cloudpipe/cloudpickle/commit/9518ae3cc71b7a6c14478a6881c0db41d73812b8    # noqa: E501
-# Please see LICENSE.third-party file for full copyright information.
-
-def _is_importable(obj):
-    """Check if an object is importable.
-    Parameters
-    ----------
-    obj :
-        Must define `__module__` and `__qualname__`.
-    """
-    if obj.__module__ in sys.modules:
-        ptr = sys.modules[obj.__module__]
-        # Walk through the attributes
-        parts = obj.__qualname__.split('.')
-        if len(parts) > 1:
-            # can't deal with function insides classes yet
-            return False
-        for p in parts:
-            try:
-                ptr = getattr(ptr, p)
-            except AttributeError:
-                return False
-        return obj is ptr
-    return False
-
-
-def _function_setstate(obj, states):
-    """The setstate function is executed after creating the function instance
-    to add `cells` into it.
-    """
-    cells = states.pop('cells')
-    for i, v in enumerate(cells):
-        _cell_set(obj.__closure__[i], v)
-    return obj
-
-
-def _reduce_function_no_cells(func, globs):
-    """_reduce_function() but return empty cells instead.
-    """
-    if func.__closure__:
-        oldcells = [cell.cell_contents for cell in func.__closure__]
-        cells = [None for _ in range(len(oldcells))] # idea from cloudpickle
-    else:
-        oldcells = ()
-        cells = None
-    rebuild_args = (_reduce_code(func.__code__), globs, func.__name__, cells,
-                    func.__defaults__)
-    return rebuild_args, oldcells
-
-
-def _cell_rebuild(contents):
-    """Rebuild a cell from cell contents
-    """
-    if contents is None:
-        return CellType()
-    else:
-        return CellType(contents)
-
     Usage:
-
     >>> def my_fn(x):
     >>>     ...
     >>> wrapped_fn = PickleCallableByPath(my_fn)
@@ -299,50 +231,3 @@ def _cell_rebuild(contents):
     @classmethod
     def _rebuild(cls, modname, fn_path):
         return cls(getattr(sys.modules[modname], fn_path))
-=======
-def _cell_set(cell, value):
-    """Set *value* into *cell* because `.cell_contents` is not writable
-    before python 3.7.
-    See https://github.com/cloudpipe/cloudpickle/blob/9518ae3cc71b7a6c14478a6881c0db41d73812b8/cloudpickle/cloudpickle.py#L298   # noqa: E501
-    """
-    if PYVERSION >= (3, 7):  # pragma: no branch
-        cell.cell_contents = value
-    else:
-        _cell_set = FunctionType(
-            _cell_set_template_code, {}, '_cell_set', (), (cell,),)
-        _cell_set(value)
-
-
-def _make_cell_set_template_code():
-    """See _cell_set"""
-    def _cell_set_factory(value):
-        lambda: cell
-        cell = value
-
-    co = _cell_set_factory.__code__
-
-    _cell_set_template_code = CodeType(
-        co.co_argcount,
-        co.co_kwonlyargcount,
-        co.co_nlocals,
-        co.co_stacksize,
-        co.co_flags,
-        co.co_code,
-        co.co_consts,
-        co.co_names,
-        co.co_varnames,
-        co.co_filename,
-        co.co_name,
-        co.co_firstlineno,
-        co.co_lnotab,
-        co.co_cellvars,  # co_freevars is initialized with co_cellvars
-        (),  # co_cellvars is made empty
-    )
-    return _cell_set_template_code
-
-
-if PYVERSION < (3, 7):
-    _cell_set_template_code = _make_cell_set_template_code()
-
-# End adapting from cloudpickle
-# ----------------------------------------------------------------------------
